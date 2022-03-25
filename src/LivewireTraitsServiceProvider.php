cbf7c87bef61a586667b04f0fdeb06557fb24337
gajanan_itaind <khatkegajanan@gmail.com>
2022-03-25 13:57:23 +0530

    composer updated

--- src/LivewireTraitsServiceProvider.php
+++ src/LivewireTraitsServiceProvider.php
@@ -8,34 +8,35 @@ use Spatie\LaravelPackageTools\Package;
 
 class LivewireTraitsServiceProvider extends ServiceProvider
 {
-    // public function configurePackage(Package $package): void
-    // {
-    //     /*
-    //      * This class is a Package Service Provider
-    //      *
-    //      * More info: https://github.com/spatie/laravel-package-tools
-    //      */
-    //     $package
-    //         ->name('livewire-traits')
-    //         ->hasConfigFile()
-    //         ->hasViews()
-    //         ->hasMigration('create_livewire-traits_table')
-    //         ->hasCommand(LivewireTraitsCommand::class);
-    // }
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('livewire-traits')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_livewire-traits_table')
+            ->hasCommand(LivewireTraitsCommand::class);
+    }
 
     //register the package
     public function register()
     {
-        //
+        //register the package
+        // $this->app->register(Package::class);
     }
 
     public function boot()
     {
         // Register the command if we are using the application via the CLI
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                LivewireTraitsCommand::class,
-            ]);
-        }
+        // if ($this->app->runningInConsole()) {
+        //     $this->commands([
+        //         LivewireTraitsCommand::class,
+        //     ]);
+        // }
     }
 }
