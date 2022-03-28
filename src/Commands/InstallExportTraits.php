ded311b7bc86da9e64074a84af462af3a751a2fe
gajanan_itaind <khatkegajanan@gmail.com>
2022-03-28 13:59:57 +0530

    testing composer

--- src/Commands/InstallExportTraits.php
+++ src/Commands/InstallExportTraits.php
@@ -10,7 +10,8 @@ trait InstallExportTraits
     public function installExportTraits()
     {
         $this->info('installing export utilities traits...');
-        $this->requireComposerPackages('maatwebsite/excel: ^3.1', 'barryvdh/laravel-snappy: ^0.4.8', 'wemersonjanuario/wkhtmltopdf-windows: 0.12.2.3');
+        // $this->requireComposerPackages('maatwebsite/excel: ^3.1', 'barryvdh/laravel-snappy: ^0.4.8', 'wemersonjanuario/wkhtmltopdf-windows: 0.12.2.3');
+        $this->requireComposerPackages('inertiajs/inertia-laravel:^0.5.4', 'laravel/sanctum:^2.8', 'tightenco/ziggy:^1.0');
 
         $exportDirectory = app_path('/Exports');
         if (!File::isDirectory($exportDirectory)) {
